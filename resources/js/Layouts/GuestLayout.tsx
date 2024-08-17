5072daf08556633e9da5844619292e361c27eaa7
Nanoka <stornblood6969@gmail.com>
2024-08-17 17:01:08 -0500

    "Updated package-lock.json, package.json, and various PHP and TSX files, including layout and icon components, routes, and views."

--- resources/js/Layouts/GuestLayout.tsx
+++ resources/js/Layouts/GuestLayout.tsx
@@ -1,19 +1,22 @@
-import ApplicationLogo from '@/Components/ApplicationLogo';
-import { Link } from '@inertiajs/react';
+import TopBar from '@/Components/TopBar';
 import { PropsWithChildren } from 'react';
 
 export default function Guest({ children }: PropsWithChildren) {
     return (
-        <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
-            <div>
-                <Link href="/">
-                    <ApplicationLogo className="w-20 h-20 fill-current text-gray-500" />
-                </Link>
-            </div>
-
-            <div className="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+        <>
+            <header>
+                <TopBar />
+            </header>
+            <main>
                 {children}
-            </div>
-        </div>
+            </main>
+            <footer>
+                <div className="py-6 bg-orange-400 dark:bg-gray-700">
+                    <p className="px-4 text-xl font-bold text-center text-gray-700 dark:text-white">
+                        Copyright © 2024 - HEAT FACTORY SAC
+                    </p>
+                </div>
+            </footer>
+        </>
     );
 }
---
+++ resources/js/Pages/Home.tsx
@@ -0,0 +1,10 @@
+import TopBar from "@/Components/TopBar"
+import GuestLayout from "@/Layouts/GuestLayout"
+
+export default function Home() {
+    return (
+        <GuestLayout>
+            <h1>Hola mundo</h1>
+        </GuestLayout>
+    )
+}
