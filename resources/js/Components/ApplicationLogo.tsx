0fab6df1f9241ba1388713d6e491a21622e9817f
Nanoka <stornblood6969@gmail.com>
2024-08-22 16:58:48 -0500

    "Replaced text-based logo with image-based logo in ApplicationLogo component"

--- resources/js/Components/ApplicationLogo.tsx
+++ resources/js/Components/ApplicationLogo.tsx
@@ -3,13 +3,9 @@ import { Link } from "@inertiajs/react";
 export default function ApplicationLogo() {
     return (
         <div className='inline-flex gap-2'>
-            <h1 className='text-2xl font-black'>
-                <Link href='/'>
-                    <span className='text-4xl text-orange-400'>H</span>EAT
-                    &nbsp;
-                    <span className='text-4xl text-orange-400'>P</span>ERÚ
-                </Link>
-            </h1>
+            <Link href='/'>
+                <img src="/logoheat.png" alt="Logo Heat Perú" />
+            </Link>
         </div>
     );
 }
