f6518217dd8cf394f2cded469db9b7aab95f945a
Nanoka <stornblood6969@gmail.com>
2024-08-29 11:35:50 -0500

    Added images on Service Page

--- resources/js/Pages/Services.tsx
+++ resources/js/Pages/Services.tsx
@@ -12,11 +12,11 @@ function ServiceItem({ id, title, text }: Service) {
     return (
 
         <div className='grid items-center grid-cols-1 gap-4 px-4 py-4 rounded-md lg:grid-cols-2'>
-            <div className={`${id % 2 === 0 ? 'lg:order-2' : 'lg:order-1'} bg-zinc-100 h-full flex flex-col justify-center p-6 md:p-12`}>
-                <h2 className='mb-4 text-2xl font-black text-center text-red-500 md:mb-8 md:text-3xl lg:text-4xl'>{title}</h2>
+            <div className={`${id % 2 === 0 ? 'lg:order-2' : 'lg:order-1'} bg-zinc-50 h-full flex flex-col justify-center p-6 md:p-12`}>
+                <h2 className='mb-4 text-2xl font-black text-center text-zinc-900 md:mb-8 md:text-3xl lg:text-4xl'>{title}</h2>
                 <p className='text-sm text-justify md:text-base text-zinc-500'>{text}</p>
             </div>
-            <img src={`/images/services/3.jpeg`} alt={title} className={`object-cover h-full aspect-video rounded-md ${id % 2 === 0 ? 'lg:order-1' : 'lg:order-2'}`} />
+            <img src={`/images/services/${id}.jpeg`} alt={title} className={`object-cover h-full aspect-video rounded-md ${id % 2 === 0 ? 'lg:order-1' : 'lg:order-2'}`} />
         </div>
     )
 }
