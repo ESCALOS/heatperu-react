2275711387903a5cf4343d80fa5724e7ab3c810d
Nanoka <stornblood6969@gmail.com>
2025-02-06 22:41:48 -0500

    Added logo and link to youtube channel

--- resources/js/Components/HeaderSocialMedia.tsx
+++ resources/js/Components/HeaderSocialMedia.tsx
@@ -1,26 +1,64 @@
-import { BiLogoFacebook, BiLogoInstagram, BiLogoLinkedin } from 'react-icons/bi'
+import {
+    BiLogoFacebook,
+    BiLogoInstagram,
+    BiLogoLinkedin,
+    BiLogoYoutube,
+} from "react-icons/bi";
 
 function HeaderSocialMedia() {
     const iconSize = 36;
     return (
-        <ul className='flex justify-around gap-4 py-8'>
+        <ul className="flex justify-around gap-4 py-8">
             <li>
-                <a href='https://www.facebook.com/heatfactoryperu' target="_blank" rel="noopener noreferrer">
-                    <BiLogoFacebook size={iconSize} className='text-gray-900 hover:text-gray-700' />
+                <a
+                    href="https://www.youtube.com/@HEATFACTORYSAC"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <BiLogoYoutube
+                        size={iconSize}
+                        className="text-gray-900 hover:text-gray-700"
+                    />
                 </a>
             </li>
             <li>
-                <a href='https://www.instagram.com/calderas_quemadores_peru' target="_blank" rel="noopener noreferrer">
-                    <BiLogoInstagram size={iconSize} className='text-gray-900 hover:text-gray-700' />
+                <a
+                    href="https://www.facebook.com/heatfactoryperu"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <BiLogoFacebook
+                        size={iconSize}
+                        className="text-gray-900 hover:text-gray-700"
+                    />
                 </a>
             </li>
             <li>
-                <a href='https://www.linkedin.com/company/heat-factory/' target="_blank" rel="noopener noreferrer">
-                    <BiLogoLinkedin size={iconSize} className='text-gray-900 hover:text-gray-700' />
+                <a
+                    href="https://www.instagram.com/calderas_quemadores_peru"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <BiLogoInstagram
+                        size={iconSize}
+                        className="text-gray-900 hover:text-gray-700"
+                    />
+                </a>
+            </li>
+            <li>
+                <a
+                    href="https://www.linkedin.com/company/heat-factory/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <BiLogoLinkedin
+                        size={iconSize}
+                        className="text-gray-900 hover:text-gray-700"
+                    />
                 </a>
             </li>
         </ul>
-    )
+    );
 }
 
-export default HeaderSocialMedia
+export default HeaderSocialMedia;
